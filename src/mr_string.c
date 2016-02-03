ed6f949671d5711b57c27f1b0454204cf4aa56ab
majorli <154788733@qq.com>
2016-02-03 20:02:46 +0800

    Updated by Libin, 20160203 20:02

--- src/mr_string.c
+++ src/mr_string.c
@@ -8,6 +8,8 @@ int utf2gb(char *src, size_t slen, char *dest, size_t dlen);
 int gb2utf(char *src, size_t slen, char *dest, size_t dlen);
 char *c2s(char *dest, const char *src);
 char *s2c(char *dest, const char *src);
+int scanChar(const char *str, const char *startp, int charset);
+int scanbChar(const char *str, const char *startp, int charset);
 
 /**
  * UTF-8字符串转换为GB18030字符串
@@ -171,3 +173,88 @@ char *s2c(char *dest, const char *src)
 	return dest;
 }
 
+/**
+ * 向后扫描一个字，支持UTF-8和GB18030两种字符集。扫描时按字符集码段规则判断是否有效的字，无效字包括不在码段规定范围内的字节或不完整的多字节字
+ *
+ * UTF-8字符集下，ASCII字符的长度为1字节，扩展字符的长度为2字节，汉字的长度为3字节，CJK扩展字符的长度为4字节，码段如下：
+ * 	ASCII		扩展字符	汉字			CJK扩展字符
+ *	00-7F		C0-DF|80-BF	E0-EF|(80-BF)*2		F0-F7|(80-BF)*3
+ * GB18030字符集下，ASCII字符的长度为1字节，汉字的长度为2字节，少数民族语言字符的长度为4字节，码段如下：
+ * 	ASCII		汉字(一)	汉字(二)	少数民族语言字符
+ * 	00-7F		81-FE|40-7E	81-FE|80-FE	81-FE|30-39|81-FE|30-39
+ *
+ * str:		被扫描的字符串
+ * startp:	扫描的起点
+ * charset:	字符集，1表示GB18030，0或其他值为UTF-8
+ * 
+ * 返回:	如果读到有效的字，返回字的字节长度；如果读到的字无效，返回无效部分的字节长度的负值；如果已经抵达字符串末尾，返回0
+ */
+int scanChar(const char *str, const char *startp, int charset)
+{
+	int ret = 0;
+	if (str != NULL && startp != NULL && *startp != EOS) {
+		const char *p = startp;
+		unsigned char c0 = *(p++);
+		unsigned char c1 = *(p++);
+		unsigned char c2 = *(p++);
+		unsigned char c3 = *p;
+		if (c0 < 0x80) {
+			ret = 1;			// ASCII字符
+		} else {
+			if (charset == GB18030) {	// GB18030
+				if (IN(c0, 0x81, 0xfe) && (IN(c1, 0x40, 0x7e) || IN(c1, 0x80, 0xFE))) {
+					ret = 2;		// GB18030双字节字符
+				} else if (IN(c0, 0x81, 0xfe) && IN(c1, 0x30, 0x39) && IN(c2, 0x81, 0xfe) && IN(c3, 0x30, 0x39)) {
+					ret = 4;		// GB18030少数民族扩展字符
+				} else {
+					ret = -2;		// 2字节无效字符
+				}
+			} else {			// UTF-8
+				if (IN(c0, 0xc0, 0xdf)) {
+					if (IN(c1, 0x80, 0xbf)) {
+						ret = 2;	// UTF-8双字节字符
+					} else {
+						ret = -2;	// 2字节无效字符
+					}
+				} else if (IN(c0, 0xe0, 0xef)) {
+					if (IN(c1, 0x80, 0xbf) && IN(c2, 0x80, 0xbf)) {
+						ret = 3;	// UTF-8汉字
+					} else {
+						ret = -3;	// 3字节无效字符
+					}
+				} else if (IN(c0, 0xf0, 0xf7)) {
+					if (IN(c1, 0x80, 0xbf) && IN(c2, 0x80, 0xbf) && IN(c3, 0x80, 0xbf)) {
+						ret = 4;	// UTF-8四字节CJK扩展字符
+					} else {
+						ret = -4;	// 4字节无效字符
+					}
+				} else {
+					ret = -1;		// 1字节无效字符
+				}
+			}
+		}
+	}
+	return ret;
+}
+
+/**
+ * 向前扫描一个字，支持UTF-8和GB18030两种字符集。扫描时按字符集码段规则判断是否有效的字，无效字包括不在码段规定范围内的字节或不完整的多字节字
+ *
+ * UTF-8字符集下，ASCII字符的长度为1字节，扩展字符的长度为2字节，汉字的长度为3字节，CJK扩展字符的长度为4字节，码段如下：
+ * 	ASCII		扩展字符	汉字			CJK扩展字符
+ *	00-7F		C0-DF|80-BF	E0-EF|(80-BF)*2		F0-F7|(80-BF)*3
+ * GB18030字符集下，ASCII字符的长度为1字节，汉字的长度为2字节，少数民族语言字符的长度为4字节，码段如下：
+ * 	ASCII		汉字(一)	汉字(二)	少数民族语言字符
+ * 	00-7F		81-FE|40-7E	81-FE|80-FE	81-FE|30-39|81-FE|30-39
+ *
+ * str:		被扫描的字符串
+ * startp:	扫描的起点
+ * charset:	字符集，1表示GB18030，0或其他值为UTF-8
+ * 
+ * 返回:	如果读到有效的字，返回字的字节长度；如果读到的字无效，返回无效部分的字节长度的负值；如果已经抵达字符串末尾，返回0
+ */
+int scanbChar(const char *str, const char *startp, int charset)
+{
+	int ret = 0;
+	return ret;
+}
